8feed06b2cafea3e53f8e757ebd99c721aed8db0
YosstinCode <yosstincode@gmail.com>
2022-11-19 13:37:02 -0500

    fixed tiempo.py change input distacion to aceleracion

--- tiempo.py
+++ tiempo.py
@@ -3,7 +3,7 @@ from utils import pedir_datos
 
 def calcular_tiempo():
 
-    datos = pedir_datos("velocidad inicial", "velocidad final", "distancia")
+    datos = pedir_datos("velocidad inicial", "velocidad final", "aceleracion")
 
     # Tiempo = (Velocidad final - Velocidad inicial) / Distancia
     return (datos[1] - datos[0]) / datos[2]
